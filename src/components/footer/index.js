0c133b39d5210a592fd4d74d216a0b0b6f2a32ec
unknown <mateus@tminformatica.com.br>
2022-12-01 12:55:13 -0300

    Ajuste no rodapé

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,13 +1,11 @@
 import styles from './styles.module.scss';
-import { AiFillHeart, AiOutlineGithub, AiOutlineLinkedin } from 'react-icons/ai';
+import { AiOutlineGithub, AiOutlineLinkedin } from 'react-icons/ai';
 
 
 function Footer() {
     return (
         <div className={styles.footer}>
-            Desenvolvido com
-            <AiFillHeart className={styles.hearticon} />
-            <a href="https://mateuscamaccho.github.io/" target="_blank">Mateus Camacho</a>
+            Desenvolvido por            <a href="https://mateuscamaccho.github.io/" target="_blank">Mateus Camacho</a>
             <a href="https://github.com/mateuscamaccho" target="_blank">
                 <AiOutlineGithub className={styles.icon} />
             </a>
